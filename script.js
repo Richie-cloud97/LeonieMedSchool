67fe4ba5b3f38691c4c91b0e4ecc371ea067d9ce
Richie-cloud97 <adjeshheralall@hotmail.com>
2026-07-25 11:41:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const button = document.getElementById("themeToggle");
 
+button.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        button.textContent = "☀️";
+    }else{
+        button.textContent = "🌙";
+    }
+});
